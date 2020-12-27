806c13bd0f18e8618834a00995f334bb62e31862
Alex-lab-design <73022364+Alex-lab-design@users.noreply.github.com>
2020-12-27 17:40:27 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-$('.button, .header__nav-list').on('click', function(){
-$('.header__nav').toggleClass('header__nav--active')
-});
-$('.button, .header__nav-list').on('click', function(){
-    $('body').toggleClass('body--active')
-    });
-});
