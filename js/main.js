68c9f17f204391b376a794a18c215de4049d9994
Alex-lab-design <73022364+Alex-lab-design@users.noreply.github.com>
2020-12-27 17:40:52 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(function(){
-$('.button').on('click', function(){
+$('.button, .header__nav-list').on('click', function(){
 $('.header__nav').toggleClass('header__nav--active')
 });
+$('.button, .header__nav-list').on('click', function(){
+    $('body').toggleClass('body--active')
+    });
 });
